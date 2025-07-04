c446a4b5dcf884503d8ba1775944852ad7ddc929
satvikapadakanti <satvikapadakanti@gmail.com>
2025-07-04 06:53:28 +0530

    added solution for longest palindrome

---
+++ longest_palindrome.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int longestPalindrome(String s) {
+        HashSet<Character> set=new HashSet<>();
+        int count=0;
+        for(char c:s.toCharArray()){
+            if(set.contains(c)){
+                count+=2;
+                set.remove(c);
+            }
+            else{
+                set.add(c);
+            }
+        }
+        if(set.size()>0) return count+1;
+        return count;
+    }
+}
